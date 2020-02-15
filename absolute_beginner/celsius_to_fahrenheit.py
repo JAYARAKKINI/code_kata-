9acf4cb044977a66db4524bbfd0d1dfb00738be3
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-15 21:30:53 +0530

    Create celsius_to_fahrenheit.py

---
+++ absolute_beginner/celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+# Python Program to convert temperature in celsius to fahrenheit
+# give the celsius value
+celsius=float(input())
+# calculate fahrenheit
+fahrenheit = (celsius* 1.8) + 32
+print('%0.2f' %(fahrenheit))
