1b483b421e3f2ac57de28d0b1f3ab81a99171931
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-15 21:21:36 +0530

    Create area_of_equilateral_tri.py

---
+++ absolute_beginner/area_of_equilateral_tri.py
@@ -0,0 +1,9 @@
+# Python Program to find Area of an Equilateral Triangle
+import math
+ 
+a= float(input(''))
+ 
+# calculate the area
+Area = (math.sqrt(3)/ 4)*(a * a)
+# print the area
+print("%.2f" %Area)         
