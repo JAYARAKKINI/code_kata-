82b16918004338a3d4d354cc8a34cc49eaa77b80
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-16 20:08:02 +0530

    Create km_to_m_and_cm.py

---
+++ absolute_beginner/km_to_m_and_cm.py
@@ -0,0 +1,8 @@
+#To convert given km value into m and cm
+
+A=int(input())  #Enter the input
+B=A*1000        #calculate M value
+C=A*100000      #calculate cm value
+#print the output
+print(B)        
+print(C)
