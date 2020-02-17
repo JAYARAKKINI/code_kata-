1e6ba70662a1fb88f66b6ac375125450a058b607
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-17 11:51:34 +0530

    Create remove_space_and_print_length.py

---
+++ absolute_beginner/remove_space_and_print_length.py
@@ -0,0 +1,5 @@
+#Remove all the whitespaces and then print the length of the remaining string.
+
+string=input()       #enter the input
+ss=string.replace(' ','')   #remove white spaces
+print(len(ss))         #print length of string
