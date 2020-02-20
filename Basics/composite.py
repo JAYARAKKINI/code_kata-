48dacf988a9c3d6e620ba0ebc736c543646e3227
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-20 22:45:35 +0530

    Create composite.py

---
+++ Basics/composite.py
@@ -0,0 +1,11 @@
+#To check the value is composite or not
+
+N=int(input())     #Enter the input
+l=0
+for i in range(1,N):  
+  if N%i==0:
+    l=i
+if l>1:
+  print('yes')        #If it is greater than 1 print yes
+else:
+  print('no')         #If it is greater than 1 print no
