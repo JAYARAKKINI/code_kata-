bc17b087720f91108eea9d89249e5c462295979e
JAYARAKKINI <37235960+JAYARAKKINI@users.noreply.github.com>
2020-02-21 11:31:08 +0530

    Create bitwise_OR.py

---
+++ Basics/bitwise_OR.py
@@ -0,0 +1,5 @@
+#To Find the Bitwise OR of the array elements.
+
+n=int(input())      #Enter the no of element
+a=list(map(int,input().split()))  #Enter the n elements
+print(a[0]|a[1])               #Print Bitwise OR of n numbers
